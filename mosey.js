5d4f3d16dc26d69b6d51fa5853e32a0d6071a8ec
Tad Thorley <phaedryx@gmail.com>
2010-10-23 14:53:08 -0600

    preliminary topic work

--- mosey.js
+++ mosey.js
@@ -2,17 +2,62 @@
   A limechat theme by Tad Thorley
 -----------------------------------------------------------------------------*/
 
-function markDuplicateTimestamps(node) {
-  var prev_time = node.previousSibling.childNodes[0];
-  var curr_time = node.childNodes[0];
-  if(prev_time.innerHTML == curr_time.innerHTML) { 
-    curr_time.className += " duptime";
+String.prototype.startsWith = function(str) {
+  return this.indexOf(str) == 0;
+}
+
+String.prototype.includes = function(str) {
+  return this.indexOf(str) >= 0;
+}
+
+function markDuplicateTimestamp(node) {
+  var prev_time_node = node.previousSibling.firstChild;
+  var curr_time_node = node.firstChild;
+  if(prev_time_node.innerHTML == curr_time_node.innerHTML) { 
+    curr_time_node.className += " duptime";
   }  
 }
 
+// limechat doesn't mark topic event messages as topic types
+// and some other type inconsistencies
+function markEvent(node) {
+  if(node.className.includes("event")) {
+    node.setAttribute("type", "event")
+  }
+  markTopicEvent(node);
+}
+
+function markTopicEvent(node) {
+  if(node.getAttribute("type") == "event") {
+    var message_node = node.lastChild;
+    if(message_node.innerText.startsWith("Topic")) {
+      message_node.setAttribute("type", "topic");
+    }
+  }
+}
+
+function createTopic() {
+  topic = document.createElement('div');
+  topic.id = "topic";
+  document.body.appendChild(topic);
+  return topic;
+}
+
+function doTopic(node) {
+  var topic = document.getElementById('topic') || createTopic();
+  var message_node = node.lastChild;
+  if(message_node.getAttribute("type") == "topic") {
+    topic.innerText = message_node.innerText.match(/opic: (.*)/)[1];
+  }
+}
+
 function processNode(ev) {
+  // TODO: differentiate between the message panel and the console panel
+  // fix some bugs
   var inserted_node = ev.target;
-  markDuplicateTimestamps(inserted_node);
+  markDuplicateTimestamp(inserted_node);
+  markEvent(inserted_node);
+  doTopic(inserted_node);
 }
 
 document.addEventListener("DOMNodeInserted", processNode, false);
